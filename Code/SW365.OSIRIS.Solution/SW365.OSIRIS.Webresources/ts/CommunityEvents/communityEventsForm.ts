4ca81d17781eb505d46a325857f7845e008d3143
sanjaya-yapa <sanjaya_yapa@hotmail.com>
2024-08-20 16:32:06 +1000

    #274 Initial changes (Incomplete)

---
+++ Code/SW365.OSIRIS.Solution/SW365.OSIRIS.Webresources/ts/CommunityEvents/communityEventsForm.ts
@@ -0,0 +1,33 @@
+﻿namespace SW365.OSIRIS.Common {
+	export interface ICommunityEventFields {
+		startDate: string;
+		endDate: string;
+		registrationSartDate: string;
+		registrationEndDate: string;
+	}
+	export class CommunityEventsForm extends SW365.OSIRIS.Common.CommonUtil {
+		static Fields: ICommunityEventFields = {
+			startDate: 'sw365_eventstartdate',
+			endDate: 'sw365_eventenddate',
+			registrationSartDate: 'sw365_registrationstartdate',
+			registrationEndDate: 'sw365_registrationenddate'
+		}
+		constructor(executionContext: Xrm.Events.EventContext) {
+			super(executionContext);
+		}
+		public validateFormDates(): void {
+			const eventStartDate = this.attribute.value(CommunityEventsForm.Fields.startDate);
+			const eventEndDate = this.attribute.value(CommunityEventsForm.Fields.endDate);
+			if (eventStartDate > eventEndDate) {
+				const alertString = { confirmButtonLabel: 'OK', text: 'Start date cannot be greater than end date.' };
+				const alertOptions = { height: 120, width: 260 }
+				Xrm.Navigation.openAlertDialog(alertString, alertOptions).then(
+					(_) => {
+						this.attribute.clearValue(CommunityEventsForm.Fields.startDate);
+					}
+				);
+			}
+		}
+	}
+	export const CommunityEvent = new CommunityEventsForm();
+}
--- Code/SW365.OSIRIS.Solution/SW365.OSIRIS.Webresources/ts/common/commonUtils.ts
+++ Code/SW365.OSIRIS.Solution/SW365.OSIRIS.Webresources/ts/common/commonUtils.ts
@@ -2,8 +2,11 @@
     interface IAttribute {
         getValue(): any;
         setValue(value: any): void;
+        clearValue(): void;
+        setRequiredLevel(requiredLevel: string): void;
         addOnChange(handler: Xrm.Events.ContextSensitiveHandler): void;
         removeOnChange(handler: Xrm.Events.ContextSensitiveHandler): void;
+        getLookupValue(): { id: string, name: string, entityType: string } | null;
         // Add more attribute methods and properties as needed
     }
 
@@ -46,11 +49,131 @@
         };
         // Add more form context  methods and properties as needed
     }
+
+    export class AttributeWrapper implements IAttribute {
+        private attribute: Xrm.Attributes.Attribute<any>;
+
+        constructor(_attribute: Xrm.Attributes.Attribute<any>) {
+            this.attribute = _attribute;
+        }
+
+        getValue(): any { return this.attribute.getValue(); }
+
+        setValue(value: any): any { return this.attribute.setValue(value); }
+
+        clearValue(): void { this.attribute.setValue(null); }
+
+        setRequiredLevel(requiredLevel: string): void {
+            this.attribute.setRequiredLevel(requiredLevel as Xrm.Attributes.RequirementLevel);
+        }
+
+        addOnChange(handler: Xrm.Events.ContextSensitiveHandler): void {
+            this.attribute.addOnChange(handler);
+        }
+
+        removeOnChange(handler: Xrm.Events.ContextSensitiveHandler): void {
+            this.attribute.removeOnChange(handler);
+        }
+
+        getLookupValue(): { id: string, name: string, entityType: string } | null {
+            const lookupValue = (this.attribute as Xrm.Attributes.LookupAttribute).getValue();
+            if (lookupValue && lookupValue.length > 0) {
+                const { id, name, entityType } = lookupValue[0];
+                return { id, name, entityType };  
+            }
+            return null;
+        }
+    }
+
+    export class FormContext implements IFormContext {
+        private formContext: Xrm.FormContext;
+
+        constructor(_formContext: Xrm.FormContext) {
+            this.formContext = _formContext;
+        }
+
+        public getAttribute(attributeName: string): IAttribute {
+            return this.formContext.getAttribute(attributeName) as IAttribute;
+        }
+        getControl(controlName: string): IControl | undefined {
+            throw new Error("Method not implemented.");
+        }
+        ui: {
+            setFormNotification(message: string, level: "ERROR" | "INFO" | "WARNING", uniqueId: string): boolean; clearFormNotification(uniqueId: string): boolean; tabs: {
+                get(tabName: string): ITab | undefined;
+            };
+        };
+        data: {
+            entity: {
+                getId(): string;
+                getEntityName(): string;
+            }; save(options?: Xrm.SaveOptions): Promise<void>;
+        };
+
+    }
     export class CommonUtil {
         protected formContext: IFormContext;
         constructor(_formContext: IFormContext) {
             this.formContext = _formContext;
         }
+
+        public attribute = {
+            value: (fieldName: string, value?: any): any => {
+                const attribute = this.formContext.getAttribute(fieldName) as IAttribute;
+
+                if (!attribute) {
+                    console.error(`Attribute ${fieldName} not found.`);
+                    return null;
+                }
+
+                if (typeof value !== 'undefined') {
+                    attribute.setValue(value);
+                    return;
+                }
+
+                return attribute.getValue();
+            },
+
+            clearValue: (fieldName: string): void => {
+                const attribute = this.formContext.getAttribute(fieldName) as IAttribute;
+
+                if (!attribute) {
+                    console.error(`Attribute ${fieldName} not found.`);
+                    return;
+                }
+
+                attribute.clearValue();
+            },
+
+            setRequiredLelvel: (fieldName: string, requiredLevel: 'none' | 'required' | 'recommended'): void => {
+                const attribute = this.formContext.getAttribute(fieldName) as IAttribute;
+
+                if (!attribute) {
+                    console.error(`Attribute ${fieldName} not found.`);
+                    return;
+                }
+
+                attribute.setRequiredLevel(requiredLevel);
+            },
+
+            getLookupValue: (fieldName: string): { id: string, name: string, entityType: string } | null => {
+                const attribute = this.formContext.getAttribute(fieldName) as IAttribute;
+                if (!attribute) {
+                    console.error(`Attribute ${fieldName} not found.`);
+                    return;
+                }
+
+                if (attribute.getValue() && attribute.getValue().length > 0) {
+                    const lookupValue = attribute.getValue()[0];
+                    return {
+                        id: lookupValue.id,
+                        name: lookupValue.name,
+                        entityType: lookupValue.entityType
+                    };
+                }
+                return null;
+            }
+        };
 
         async callCustomApi(apiName: string, data: any): Promise<any> {
             const requestUrl = `${Xrm.Utility.getGlobalContext().getClientUrl()}/api/data/v9.1/${apiName}`;
